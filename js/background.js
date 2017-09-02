5ebf1b288b074b3cd7a632cefd3f99c7d20ce054
Tim Leland <timothy.leland@gmail.com>
2017-09-01 22:54:39 -0400

    Updates for drive ui changes

--- js/background.js
+++ js/background.js
@@ -1,25 +1,31 @@
-var checkForVideo = function() {
-    var videoElement = document.getElementsByClassName('slide-body video');
-    if (videoElement.length > 1 && videoElement[0].offsetWidth > 0 && videoElement[0].offsetHeight > 0) {
-        var dialog = document.getElementsByClassName('airy-video-unsupported-dialog');
-        if (dialog.length == 1) {
-            dialog[0].parentNode.removeChild(dialog[0]);
+$(function() {
+
+    var injectVideo = function(videoUrl) {
+        $('body').prepend(`
+            <div id="longVideo" style="position:fixed;top:0;left:0;right:0;bottom:0;z-index:999;width:100%;height:100%;background-color: black;">
+                <div style="padding: 10px;">
+                    <a href="${videoUrl}" target="_blank">${videoUrl}</a>
+                    <a href="javascript:void(0)" style="float: right;" id="closeLargeVideo">Close</a>
+                </div>
+                    <video autoplay width="100%" height="90%" controls>
+                        <source src="${videoUrl}" type="video/mp4">
+                    </video>
+            </div>`);
+    };
+
+    var checkForVideo = function() {
+        if ($('.VideoTooLargeError').is(':visible') || $('.VideoTooLongError').is(':visible')) {
+            $('.VideoTooLargeError .options .button, .VideoTooLongError .options .button').click();
+
+            var videoUrl = $('.ext-video:not(.prev-node, .next-node, .preloaded-node) .preload-image img').attr('src');
+            videoUrl = videoUrl.split('?')[0]; //clean url
+            injectVideo(videoUrl);
         }
+    };
 
-        var controls = document.getElementsByClassName('airy-controls-container');
+    setInterval(checkForVideo, 500);
 
-        if (controls.length == 1) {
-            controls[0].style.opacity = 100;
-            controls[0].style.visibility = 'visible';
-        }
-
-        var videoElement = document.getElementsByTagName('video')[0];
-        var newSrc = videoElement.getAttribute('src');
-        if (newSrc && newSrc.includes('/alt/video?y=720')) {
-            newSrc = newSrc.replace('/alt/video?y=720', '');
-            videoElement.setAttribute('src', newSrc);
-        }
-    }
-};
-
-setInterval(checkForVideo, 500);
+    $(document).on('click', '#closeLargeVideo', function() {
+        $('#longVideo').remove();
+    })
+});
